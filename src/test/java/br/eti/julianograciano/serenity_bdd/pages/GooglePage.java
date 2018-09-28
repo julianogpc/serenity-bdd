9263ecfe3d696a0a3148dca130fa5d2366c1e96b
Juliano Graciano <julianogpc@gmail.com>
2018-09-27 22:14:05 -0300

    Update GooglePage.java

--- src/test/java/br/eti/julianograciano/serenity_bdd/pages/GooglePage.java
+++ src/test/java/br/eti/julianograciano/serenity_bdd/pages/GooglePage.java
@@ -20,7 +20,7 @@ public class GooglePage extends BasePage {
     @FindBy(name = "q")
     private WebElement searchField;
 
-    @FindBy(name = "btnK")
+    @FindBy(id = "btnK")
     private WebElement searchButton;
 
     public GooglePage(WebDriver driver) {
